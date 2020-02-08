6c4933083406675a9c42791063d1e6bddc0a9013
heatherdiruscio <diruscih@oregonstate.edu>
2020-02-08 00:29:02 -0800

    WIP Flaskification of site - caution, some things broken! New pages: privacy policy, genres, search, about Known issues: so many...

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
-import os
 from flask import Flask, render_template
+
+from bookData import Books
+from genreData import Genres
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return render_template('template.html')
+# Save the return value of Books and Genres to new lists variables
+Books = Books()
+Genres = Genres()
 
-@app.route('/book')
-def book():
-    return render_template('book.html')
+# first route for homepage
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/genres')
+def genres():
+    return render_template('genres.html', genres=Genres, books=Books)
+
+@app.route('/genre/<string:id>/')
+def genre(id):
+    return render_template('genre.html', id=int(id), genres=Genres, books=Books)
+
+@app.route('/books')
+def books():
+    return render_template('books.html', books=Books)
+
+@app.route('/book/<string:id>/')
+def book(id):
+    return render_template('book.html', id=id, book=Books[int(id)-1]) # I did some fancy bullshit and it worked??
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.errorhandler(404)
+def error(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
---
+++ genreData.py
@@ -0,0 +1,88 @@
+def Genres():
+    genres = [
+        {
+            'id': 1,
+            'name': 'Art'
+        },
+        {
+            'id': 2,
+            'name': 'Biography'
+        },
+        {
+            'id': 3,
+            'name': 'Business'
+        },
+        {
+            'id': 4,
+            'name': 'Children\'s Books'
+        },
+        {
+            'id': 5,
+            'name': 'Classics'
+        },
+        {
+            'id': 6,
+            'name': 'Cookbooks'
+        },
+        {
+            'id': 7,
+            'name': 'Fantasy'
+        },
+        {
+            'id': 8,
+            'name': 'Fiction'
+        },
+        {
+            'id': 9,
+            'name': 'Historical Fiction'
+        },
+        {
+            'id': 10,
+            'name': 'History'
+        },
+        {
+            'id': 11,
+            'name': 'Memoir'
+        },
+        {
+            'id': 12,
+            'name': 'Music'
+        },
+        {
+            'id': 13,
+            'name': 'Mystery & Thriller'
+        },
+        {
+            'id': 14,
+            'name': 'Nonfiction'
+        },
+        {
+            'id': 15,
+            'name': 'Poetry'
+        },
+        {
+            'id': 16,
+            'name': 'Romance'
+        },
+        {
+            'id': 17,
+            'name': 'Science'
+        },
+        {
+            'id': 18,
+            'name': 'Science Fiction'
+        },
+        {
+            'id': 19,
+            'name': 'Self Help'
+        },
+        {
+            'id': 20,
+            'name': 'Travel'
+        },
+        {
+            'id': 21,
+            'name': 'Young Adult'
+        }
+    ]
+    return genres
