ec9b5043c885e351263c643aaa93d4090a6b470e
heatherdiruscio <diruscih@oregonstate.edu>
2020-02-08 18:39:41 -0800

    Attempting Heroku deploy of flip version of site

---
+++ starter_website/sample_book_data/bookData.py
@@ -0,0 +1,103 @@
+def Books():
+    books = [
+        {
+            'id': 1,
+            'isbn': '0399255373',
+            'title':'The Day the Crayons Quit',
+            'genre_id': 4,
+            'genre': 'Children\'s Books',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 1,
+            'author': 'Drew Daywalt',
+            'year_published': 2013
+        },
+        {
+            'id': 2,
+            'isbn': '0765378388',
+            'title':'The Calculating Stars: A Lady Astronaut Novel',
+            'genre_id': 18,
+            'genre': 'Science Fiction',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 2,
+            'author': 'Mary Robinette Kowal',
+            'year_published': 2018
+        },
+        {
+            'id': 3,
+            'isbn': '1476753830',
+            'title':'Salt, Fat, Acid, Heat: Mastering the Elements of Good Cooking',
+            'genre_id': 6,
+            'genre': 'Cookbooks',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 3,
+            'author': 'Samin Nosrat',
+            'year_published': 2017
+        },
+        {
+            'id': 4,
+            'isbn': '1416590323',
+            'title':'Frederick Douglass: Prophet of Freedom',
+            'genre_id': 2,
+            'genre': 'Biography',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 4,
+            'author': 'David W. Blight',
+            'year_published': 2020
+        },
+        {
+            'id': 5,
+            'isbn': '0297859382',
+            'title':'Gone Girl',
+            'genre_id': 13,
+            'genre': 'Mystery & Thriller',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 5,
+            'author': 'Gillian Flynn',
+            'year_published': 2012
+        },
+        {
+            'id': 6,
+            'isbn': '0984782850',
+            'title':'Cracking the Coding Interview: 189 Programming Questions and Solutions',
+            'genre_id': 3,
+            'genre': 'Business',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 6,
+            'author': 'Gayle Laakmann McDowell',
+            'year_published': 2015
+        },
+        {
+            'id': 7,
+            'isbn': '1608468569',
+            'title': 'Electric Arches',
+            'genre_id': 15,
+            'genre': 'Poetry',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 7,
+            'author': 'Eve L. Ewing',
+            'year_published': 2017
+        },
+        {
+            'id': 8,
+            'isbn': '1512308056',
+            'title': 'Frankenstein',
+            'genre_id': 5,
+            'genre': 'Classics',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 8,
+            'author': 'Mary Shelley',
+            'year_published': 1818
+        },
+        {
+            'id': 9,
+            'isbn': '9780141439518',
+            'title': 'Pride and Prejudice',
+            'genre_id': 5,
+            'genre': 'Romance',
+            'blurb':'Inner cover content will go here, a tale of struggle, love, and incompetence with a bittersweet resolution, this is definitely the book you should buy your mom for mother\'s day or whatever',
+            'author_id': 9,
+            'author': 'Jane Austen',
+            'year_published': 1813
+        },
+    ]
+    return books
---
+++ starter_website/webapp.py
@@ -0,0 +1,140 @@
+from flask import Flask, render_template
+from flask import request, redirect
+# from db_connector.db_connector import connect_to_database, execute_query
+
+# Pseudo-database for now
+from sample_book_data.bookData import Books
+from sample_book_data.genreData import Genres
+# Save the return value of Books and Genres to new lists variables
+Books = Books()
+Genres = Genres()
+
+#create the web application
+webapp = Flask(__name__)
+
+@webapp.route('/')
+def index():
+    return render_template('home.html')
+
+@webapp.route('/about')
+def about():
+    return render_template('about.html')
+
+@webapp.route('/books')
+def books():
+    return render_template('books.html', books=Books)
+
+@webapp.route('/book/<string:id>/')
+def book(id):
+    return render_template('book.html', id=id, book=Books[int(id)-1]) # I did some fancy bullshit and it worked??
+
+@webapp.route('/genres')
+def genres():
+    return render_template('genres.html', genres=Genres, books=Books)
+
+@webapp.route('/genre/<string:id>/')
+def genre(id):
+    return render_template('genre.html', id=int(id), genres=Genres, books=Books)
+
+@webapp.route('/search')
+def search(): # view functions must have unique names or everything breaks
+    return render_template('search.html')
+
+@webapp.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@webapp.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+# @webapp.route('/diagnostic')
+# def fetch_diagnostic():
+#     print("Fetching and rendering diagnostic")
+#     db_connection = connect_to_database()
+#     query = "SELECT * from diagnostic;"
+#     result = execute_query(db_connection, query).fetchall();
+#     print(result)
+#     return render_template('diagnostic_page.html', diagnostic_message=result)
+
+# @webapp.route('/browse_bsg_people')
+# def browse_people():
+#     print("Fetching and rendering people web page")
+#     db_connection = connect_to_database()
+#     query = "SELECT fname, lname, homeworld, age from bsg_people;"
+#     result = execute_query(db_connection, query).fetchall();
+#     print(result)
+#     return render_template('people_browse.html', rows=result)
+
+# @webapp.route('/add_new_people', methods=['POST','GET'])
+# def add_new_people():
+#     db_connection = connect_to_database()
+#     if request.method == 'GET':
+#         query = 'SELECT planet_id, name from bsg_planets'
+#         result = execute_query(db_connection, query).fetchall();
+#         print(result)
+#
+#         return render_template('people_add_new.html', planets = result)
+#     elif request.method == 'POST':
+#         print("Add new people!");
+#         fname = request.form['fname']
+#         lname = request.form['lname']
+#         age = request.form['age']
+#         homeworld = request.form['homeworld']
+#
+#         query = 'INSERT INTO bsg_people (fname, lname, age, homeworld) VALUES (%s,%s,%s,%s)'
+#         data = (fname, lname, age, homeworld)
+#         execute_query(db_connection, query, data)
+#         return ('Person added!');
+#
+# @webapp.route('/db-test')
+# def test_database_connection():
+#     print("Executing a sample query on the database using the credentials from db_credentials.py")
+#     db_connection = connect_to_database()
+#     query = "SELECT * from bsg_people;"
+#     result = execute_query(db_connection, query);
+#     return render_template('db_test.html', rows=result)
+#
+# #display update form and process any updates, using the same function
+# @webapp.route('/update_people/<int:id>', methods=['POST','GET'])
+# def update_people(id):
+#     db_connection = connect_to_database()
+#     #display existing data
+#     if request.method == 'GET':
+#         people_query = 'SELECT character_id, fname, lname, homeworld, age from bsg_people WHERE character_id = %s' % (id)
+#         people_result = execute_query(db_connection, people_query).fetchone()
+#
+#         if people_result == None:
+#             return "No such person found!"
+#
+#         planets_query = 'SELECT planet_id, name from bsg_planets'
+#         planets_results = execute_query(db_connection, planets_query).fetchall();
+#
+#         return render_template('people_update.html', planets = planets_results, person = people_result)
+#     elif request.method == 'POST':
+#         print("Update people!");
+#         character_id = request.form['character_id']
+#         fname = request.form['fname']
+#         lname = request.form['lname']
+#         age = request.form['age']
+#         homeworld = request.form['homeworld']
+#
+#         print(request.form);
+#
+#         query = "UPDATE bsg_people SET fname = %s, lname = %s, age = %s, homeworld = %s WHERE character_id = %s"
+#         data = (fname, lname, age, homeworld, character_id)
+#         result = execute_query(db_connection, query, data)
+#         print(str(result.rowcount) + " row(s) updated");
+#
+#         return redirect('/browse_bsg_people')
+# 
+# @webapp.route('/delete_people/<int:id>')
+# def delete_people(id):
+#     '''deletes a person with the given id'''
+#     db_connection = connect_to_database()
+#     query = "DELETE FROM bsg_people WHERE character_id = %s"
+#     data = (id,)
+#
+#     result = execute_query(db_connection, query, data)
+#     return (str(result.rowcount) + "row deleted")
