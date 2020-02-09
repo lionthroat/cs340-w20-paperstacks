8b155030a2c32e269ecde9f2f84837ab7233f817
heatherdiruscio <diruscih@oregonstate.edu>
2020-02-08 19:01:24 -0800

    Think app crashes without Heroku-specific PORT declaration. Testing...

--- app.py
+++ app.py
@@ -13,8 +13,16 @@ from flask import request, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('home.html')
+def hello():
+    return "<h1>Ok, kinda working...</h1>"
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    
+# def index():
+#     return render_template('home.html')
 
 # @app.route('/about')
 # def about():
