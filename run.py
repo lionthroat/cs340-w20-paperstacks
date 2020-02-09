5906f00cef4538a771d6eecf575d6244f9eecb70
heatherdiruscio <diruscih@oregonstate.edu>
2020-02-08 19:17:03 -0800

    Simple HTML working, so adding Bootstrap back in

--- run.py
+++ run.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
-# def hello_world():
-#     return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
@@ -21,18 +19,11 @@ if __name__ == '__main__':
 # # Books = Books()
 # # Genres = Genres()
 #
-# #create the web application
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def hello():
-#     return "<h1>Ok, kinda working...</h1>"
-#
 # if __name__ == '__main__':
 #     # Bind to PORT if defined, otherwise default to 5000.
 #     port = int(os.environ.get('PORT', 5000))
 #     app.run(host='0.0.0.0', port=port, debug=True)
-
+#
 # @app.route('/about')
 # def about():
 #     return render_template('about.html')
