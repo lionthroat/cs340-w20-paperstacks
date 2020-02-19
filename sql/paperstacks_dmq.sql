7ac26c6c3f68a0407bf4713a6a7f9d2d09097b5e
Kayla Smith <50057385+smithkaylar@users.noreply.github.com>
2020-02-18 22:34:44 -0700

    added more potential queries

--- sql/paperstacks_dmq.sql
+++ sql/paperstacks_dmq.sql
@@ -1,4 +1,4 @@
-
+-- READ QUERIES --
 -- Get all genre names for listing in add book, home page, and genre list page
 select genre_name from Genres;
 
@@ -9,9 +9,11 @@ select author_name from Authors;
 -- Get all book names for listing in book list and add author page
 select book_title from Books;
 
--- Get all ratings for a given book
+-- Get average rating for a given book
+select star_rating from Ratings where isbn = :ISBN_selected_from_book_page;
 
 -- Get all reviews for a given book
+select review_date, review_content from Reviews where isbn = :ISBN_selected_from_book_page;
 
 -- Get all books for a given author
 
@@ -30,3 +32,53 @@ select book_title from Books;
 -- Get search results from submitted search page
 
 -- Get search results from simple search in navbar
+
+-- ADD QUERIES --
+
+-- Add book/author relationship --
+
+-- Add book/genre relationship --
+
+-- Add rating/review relationship --
+
+-- Book: add review --
+
+-- Book: add rating --
+
+-- Genre: add genre --
+
+-- Author: add author --
+
+-- Book: add book --
+
+-- UPDATE QUERIES --
+
+-- Book: update title --
+
+-- Book: update ISBN --
+
+-- Author: update description --
+
+-- Rating: update star rating --
+
+-- Rating/review unlink --
+
+-- Review: update content --
+
+-- Genre: update genre name --
+
+-- unlink author/book --
+
+-- unlink book/genre --
+
+-- DELETE QUERIES --
+
+-- delete book --
+
+-- delete author --
+
+-- delete genre --
+
+-- delete rating --
+
+-- delete review --
