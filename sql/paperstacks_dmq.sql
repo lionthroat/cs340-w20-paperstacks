d023ab72ffbae450c744ff97300bbbc9d8f59e34
Kayla Smith <50057385+smithkaylar@users.noreply.github.com>
2020-02-18 21:12:13 -0700

    Created and started dmq file

---
+++ sql/paperstacks_dmq.sql
@@ -0,0 +1,32 @@
+
+-- Get all genre names for listing in add book, home page, and genre list page
+select genre_name from Genres;
+
+
+-- Get all author names for listing in add book and author list page
+select author_name from Authors;
+
+-- Get all book names for listing in book list and add author page
+select book_title from Books;
+
+-- Get all ratings for a given book
+
+-- Get all reviews for a given book
+
+-- Get all books for a given author
+
+-- Get all authors for a given book
+
+-- Get all books in a given genre
+
+-- Show all info for given author
+
+-- Show all info for given book
+
+-- Get associated rating for a review
+
+-- Get associated review for a rating
+
+-- Get search results from submitted search page
+
+-- Get search results from simple search in navbar
